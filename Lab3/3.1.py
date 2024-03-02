c4360b3d581e9667e74705bb7ff844566fd52ce3
Rum1837 <161057597+Rum1837@users.noreply.github.com>
2024-03-02 10:08:31 +0300

    Add files via upload

---
+++ Lab3/3.1.py
@@ -0,0 +1,13 @@
+counter = 0
+answer = input("Столица России")
+if answer == "Москва" or answer == "москва"
+    counter += 1
+    print("Вы ответили верно")
+
+answer = input("Какой язык  мы изучаем?\n")
+if answer == "Python" or answer == "python"
+    counter += 1
+    print("Вы ответили верно")
+else:
+    print("Вы ответили не верно")
+print("Вы набрали баллов {counter}")
